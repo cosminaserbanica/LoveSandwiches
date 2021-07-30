d444269acb14ef7846769733db6b2262701bc202
cosminaserbanica <cosmina.serbanica@yahoo.com>
2021-07-30 22:48:40 +0100

    Comment out the unused update worksheet functions

--- run.py
+++ run.py
@@ -53,19 +53,19 @@ def validate_data(values):
   """
   Update sales worksheet, add new row with the list data provided.
   """
-  print("Updating sales worksheet...\n")
-  sales_worksheet = SHEET.worksheet("sales")
-  sales_worksheet.append_row(data)
-  print("Sales worksheet updated succesfully!\n")
+  #print("Updating sales worksheet...\n")
+  #sales_worksheet = SHEET.worksheet("sales")
+  #sales_worksheet.append_row(data)
+  #print("Sales worksheet updated succesfully!\n")
 
 #def update_surplus_worksheet(data):
   """
   Update surplus worksheet, add new row with the list data provided.
   """
-  print("Updating surplus worksheet...\n")
-  surplus_worksheet = SHEET.worksheet("surplus")
-  surplus_worksheet.append_row(data)
-  print("Surplus worksheet updated succesfully!\n")
+  #print("Updating surplus worksheet...\n")
+  #surplus_worksheet = SHEET.worksheet("surplus")
+  #surplus_worksheet.append_row(data)
+  #print("Surplus worksheet updated succesfully!\n")
 
 def update_worksheet(data, worksheet):
   """
